5bca4de796fa705bf5de488b4ff7e55968e6682a
Slava Obukhov <slava.obukhov@soniox.com>
2026-01-23 11:26:47 +0100

    Update express example

--- examples/node/express/server.ts
+++ examples/node/express/server.ts
@@ -1,17 +1,25 @@
-import type { SonioxNodeOptions } from '@soniox/node';
+import type { SonioxNodeClientOptions } from '@soniox/node';
 import { SonioxNodeClient } from '@soniox/node';
 import express from 'express'
 
 const app = express()
 const port = 3000
 
-const sonioxOptions: SonioxNodeOptions = {}
+const sonioxOptions: SonioxNodeClientOptions = {}
 
-const soniox = new SonioxNodeClient();
+const soniox = new SonioxNodeClient(sonioxOptions);
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
+app.get('/tmp-key', async (req, res) => {
+    const key = await soniox.auth.createTemporaryKey({
+        usage_type: 'transcribe_websocket',
+        expires_in_seconds: 3600,
+    });
+    res.json(key);
+});
 
 app.listen(port, () => {
     console.log(`Soniox Express Example app listening on port ${port}`)
