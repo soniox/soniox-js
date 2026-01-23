b1fa3906c0c9e0725dd7db87c6111636ac8a95d4
Slava Obukhov <slava.obukhov@soniox.com>
2026-01-23 09:35:34 +0100

    Remove soniox/core from web

--- packages/web/src/index.ts
+++ packages/web/src/index.ts
@@ -4,8 +4,5 @@
  * Official Soniox SDK for Web browsers
  */
 
-// Re-export core types and errors
-// export * from '@soniox/core';
-
 // Web-specific exports
 export { SonioxWebClient } from './client.js';
